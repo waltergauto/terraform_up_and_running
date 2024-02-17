05c6056f72f2fa66e1792683c40b43562c9c43b0
Walter Gauto <waltergautofcb@gmail.com>
2024-02-17 04:44:17 -0300

    Add webserver deploy and aws_security_group deploy to main.tf

--- chapter_2/main.tf
+++ chapter_2/main.tf
@@ -11,13 +11,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Deploy a security group to allow port 8080
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
 ### Deploy an Amazon Linux 2023 AMI
 resource "aws_instance" "example"{
   ami = "ami-0e731c8a588258d0d"
   instance_type = "t2.micro"
+  # Pass security group instance created before to ec2 instance
+  vpc_security_group_ids = [aws_security_group.instance.id]
  
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.xhtml
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  user_data_replace_on_change = true
+
   tags = {
     Name = "example-tf"
   }
+
 }
 
