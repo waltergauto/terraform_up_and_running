fafd5d2597ce3be84e9f7d9b25f69a9ca603c185
Walter Gauto <waltergautofcb@gmail.com>
2024-02-25 18:35:58 -0300

    Configure a cluster of web servers

--- chapter_2/main.tf
+++ chapter_2/main.tf
@@ -11,36 +11,72 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 # Deploy a security group to allow port 8080
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
 
-### Deploy an Amazon Linux 2023 AMI
-resource "aws_instance" "example"{
-  ami = "ami-06aa3f7caf3a30282"
+resource "aws_launch_configuration" "example"{
+  image_id = "ami-06aa3f7caf3a30282"
   instance_type = "t2.micro"
-  # Pass security group instance created before to ec2 instance
-  vpc_security_group_ids = [aws_security_group.instance.id]
+ # Pass security group instance created before to ec2 instance
+  security_groups = [aws_security_group.instance.id]
  
   user_data = <<-EOF
               #!/bin/bash
               busybox echo "Hello, World!" >> index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  user_data_replace_on_change = true
 
-  tags = {
-    Name = "example-tf"
+  lifecycle {
+    create_before_destroy = true
   }
-
 }
 
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 3
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_instances" "web_instances" {
+  instance_state_names = ["running"]
+}
+
+output "public_ip" {
+  value = data.aws_instances.web_instances.public_ips
+  description = "The public IP address of the web server"
+}
