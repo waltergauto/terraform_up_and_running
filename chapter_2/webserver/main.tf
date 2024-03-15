459c9841f52dcb5a1bab967a53e857bae66757da
Walter Gauto <waltergautofcb@gmail.com>
2024-03-15 19:54:20 -0300

    Add webserver deploy

---
+++ chapter_2/webserver/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.37.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-06aa3f7caf3a30282"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+            #!/bin/bash
+            busybox echo "Hello, World!" >> index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
+  
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+""
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP of the Instance"
+}
