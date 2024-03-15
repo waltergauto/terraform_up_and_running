ffe1750938c22a61a30e942c60b3223f5be850c1
Walter Gauto <waltergautofcb@gmail.com>
2024-03-15 17:44:51 -0300

    Add port 22 to ingress

--- chapter_2/main.tf
+++ chapter_2/main.tf
@@ -38,7 +38,12 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_launch_configuration" "example"{
